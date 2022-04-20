af6158ed2235f2be7bed05a0dcc68bb996ce958e
sandratoo <toosandraa@gmail.com>
2022-04-20 08:02:37 +0300

    first commit

---
+++ journal/__init__.py
@@ -0,0 +1,14 @@
+from sys import prefix
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config ["SECRET_KEY"] = "secretkey"
+
+    from .views import views
+    app.register_blueprint(views)
+
+    from .auth import auth
+    app.register_blueprint(auth, url_prefix="/auth/")
+
+    return app
